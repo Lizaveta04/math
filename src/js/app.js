414296cd1b25f41eb55ce3d4ab2c91a9aff275a0
Elizaveta Kuzmicheva <kuzm_el@mail.ru>
2022-02-12 14:15:17 +0300

    Update

--- src/js/app.js
+++ src/js/app.js
@@ -7,12 +7,10 @@ console.log('app worked');
 const magician = new Magician('Маг', 'magician');
 const daemon = new Daemon('Демон', 'daemon');
 
-magician._attack = 50;
 magician.distance = 5;
-magician._stoned = false;
-console.log(magician._attack);
+magician.stoned = false;
+console.log(magician.attack);
 
-daemon._attack = 50;
 daemon.distance = 5;
-daemon._stoned = true;
-console.log(daemon._attack);
+daemon.stoned = true;
+console.log(daemon.attack);
--- src/js/magician.js
+++ src/js/magician.js
@@ -3,6 +3,6 @@ import MathCharacter from './basic';
 export default class Magician extends MathCharacter {
   constructor(name, type) {
     super(name, 'magician');
-    // this._attack = 50;
+    this._attack = 50;
   }
 }
